024be277cf4b81760ed4de2a75e55fda6b9dd945
aradburn <a.radburn@btinternet.com>
2024-02-13 22:12:32 +0000

    Stop nodes flashing black on update.

--- discograph/source/js/network/node.js
+++ discograph/source/js/network/node.js
@@ -1,6 +1,6 @@
 NODE_DEBOUNCE_TIME = 250
 NODE_OUT_TRANSITION_TIME = 500
-NODE_UPDATE_TRANSITION_TIME = 1000
+NODE_UPDATE_TRANSITION_TIME = 5000
 NODE_PALETTE = "Palette3"
 
 /* Initialize node tooltip */
@@ -18,28 +18,11 @@ function dg_network_onNodeEnter(nodeEnter) {
         .attr("class", function(d) {
             var classes = [
                 "node",
-//                d.key,
                 d.key.split('-')[0],
                 NODE_PALETTE,
             ];
             return classes.join(" ");
         })
-//        .attr("class", function(d) {
-//            if (d.type == 'artist') {
-//                return dg_color_heatmap_class(d);
-//            } else {
-//                return dg_color_greyscale_class(d);
-//            }
-//        })
-//        .style("fill", function(d) {
-//            if (d.type == 'artist') {
-//                return dg_color_heatmap(d);
-//            } else {
-//                return dg_color_greyscale(d);
-//            }
-//        })
-//        .attr("data-tooltip", "tooltip")
-//        .attr("title", function(d) { return d.name; })
         .call(d3.drag()
             .on("start", dg_network_dragstarted)
             .on("drag", dg_network_dragged)
@@ -139,8 +122,8 @@ function dg_network_onNodeExit(nodeExit) {
 
 function dg_network_onNodeUpdate(nodeUpdate) {
     nodeUpdate.selectAll(".outer")
-        .transition()
-        .duration(NODE_UPDATE_TRANSITION_TIME)
+//        .transition()
+//        .duration(NODE_UPDATE_TRANSITION_TIME)
         .attr("class", function(d) {
             var classes = [
                 "outer",
@@ -149,8 +132,8 @@ function dg_network_onNodeUpdate(nodeUpdate) {
             return classes.join(" ");
         })
     nodeUpdate.selectAll(".inner")
-        .transition()
-        .duration(NODE_UPDATE_TRANSITION_TIME)
+//        .transition()
+//        .duration(NODE_UPDATE_TRANSITION_TIME)
         .attr("class", function(d) {
             var classes = [
                 "inner",
@@ -158,34 +141,37 @@ function dg_network_onNodeUpdate(nodeUpdate) {
             ];
             return classes.join(" ");
         })
-    nodeUpdate.selectAll(".more").each(function(d, i) {
-        var prevMissing = Boolean(d.hasMissing);
-        var prevMissingByPage = Boolean(d.hasMissingByPage);
-        var currMissing = Boolean(d.missing);
-        if (!d.missingByPage) {
-            var currMissingByPage = false;
-        } else {
-            var currMissingByPage = Boolean(
-                d.missingByPage[dg.network.pageData.currentPage]
-                );
-        }
-        d3.select(this)
-            .transition()
-            .duration(NODE_UPDATE_TRANSITION_TIME)
-            .style('opacity', function(d) {
-                return (currMissing || currMissingByPage) ? 1 : 0;
-                })
-            .attrTween('transform', function(d) {
-                var start = prevMissingByPage ? 45 : 0;
-                var stop = currMissingByPage ? 45 : 0;
-                return d3.interpolateString(
-                    "rotate(" + start + ")",
-                    "rotate(" + stop + ")"
-                    );
-                });
-        d.hasMissing = currMissing;
-        d.hasMissingByPage = currMissingByPage;
-    });
+    nodeUpdate.selectAll(".more")
+        .style("opacity", function(d) {return d.missing > 0 ? 1 : 0; });
+
+//    nodeUpdate.selectAll(".more").each(function(d, i) {
+//        var prevMissing = Boolean(d.hasMissing);
+//        var prevMissingByPage = Boolean(d.hasMissingByPage);
+//        var currMissing = Boolean(d.missing);
+//        if (!d.missingByPage) {
+//            var currMissingByPage = false;
+//        } else {
+//            var currMissingByPage = Boolean(
+//                d.missingByPage[dg.network.pageData.currentPage]
+//                );
+//        }
+//        d3.select(this)
+//            .transition()
+//            .duration(NODE_UPDATE_TRANSITION_TIME)
+//            .style('opacity', function(d) {
+//                return (currMissing || currMissingByPage) ? 1 : 0;
+//                })
+//            .attrTween('transform', function(d) {
+//                var start = prevMissingByPage ? 45 : 0;
+//                var stop = currMissingByPage ? 45 : 0;
+//                return d3.interpolateString(
+//                    "rotate(" + start + ")",
+//                    "rotate(" + stop + ")"
+//                    );
+//                });
+//        d.hasMissing = currMissing;
+//        d.hasMissingByPage = currMissingByPage;
+//    });
 }
 
 function dg_network_onNodeMouseOver(event, d) {
