97f0a87d01b14339abb63d1e50dfd1e878edca0c
aradburn <a.radburn@btinternet.com>
2024-11-16 10:11:56 +0000

    Add from_str.

--- discograph/library/fields/entity_type.py
+++ discograph/library/fields/entity_type.py
@@ -5,6 +5,15 @@ class EntityType(enum.Enum):
     ARTIST = 1
     LABEL = 2
 
+    @staticmethod
+    def from_str(entity_type_str: str):
+        if entity_type_str in ("artist", "ARTIST"):
+            return EntityType.ARTIST
+        elif entity_type_str in ("label", "LABEL"):
+            return EntityType.LABEL
+        else:
+            raise NotImplementedError
+
     def __lt__(self, other):
         if self.__class__ is other.__class__:
             return self.value < other.value
