aa9d2c8414ca4aa8ece225a0d31bec84ddac096f
aradburn <a.radburn@btinternet.com>
2024-11-16 10:11:27 +0000

    Add PyDantic domain Roles for building the Roles JSTree.

--- discograph/library/domain/role.py
+++ discograph/library/domain/role.py
@@ -1,6 +1,10 @@
 __all__ = [
     "RoleUncommited",
     "Role",
+    "RoleJSTree",
+    "RoleJSTreeEntry",
+    "RoleJSTreeState",
+    "RoleJSTreeWrapper",
 ]
 
 from discograph.library.domain.base import InternalDomainObject
@@ -25,3 +29,29 @@ class Role(_RoleBase):
     """Saved Role representation."""
 
     id: int
+
+
+class RoleJSTreeState(InternalDomainObject):
+    opened: bool  # is the node open
+    disabled: bool  # is the node disabled
+    selected: bool  # is the node selected
+
+
+class RoleJSTreeEntry(InternalDomainObject):
+    id: str  # required
+    parent: str  # required
+    text: str  # node text
+    icon: str | None = None  # for custom icon
+    state: RoleJSTreeState
+    li_attr: dict  # attributes for the generated LI node
+    a_attr: dict  # attributes for the generated A node
+
+
+class RoleJSTree(InternalDomainObject):
+    data: list[RoleJSTreeEntry] = []
+
+
+class RoleJSTreeWrapper(InternalDomainObject):
+    core: RoleJSTree
+    checkbox: dict
+    plugins: list[str]
