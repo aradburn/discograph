df2241ec9e6db0fd92b55c6b8f272f444b701c48
aradburn <a.radburn@btinternet.com>
2025-03-23 14:16:51 +0000

    Add Vite configuration for asset building and project structure

---
+++ discograph/vite.config.js
@@ -0,0 +1,42 @@
+import path from "node:path";
+
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: path.join(__dirname, "./source/"),
+  base: "/assets/",
+//  resolve: {
+//    alias: {
+//      "bootstrap-icons/": path.resolve(__dirname, "./node_modules/bootstrap-icons/font"),
+//    },
+//  },
+  build: {
+    outDir: path.join(__dirname, "./source_compiled/"),
+    manifest: "manifest.json",
+    assetsDir: "bundled",
+    rollupOptions: {
+      input: [
+        "source/js/color.js",
+        "source/js/loading.js",
+        "source/js/network/events.js",
+        "source/js/network/forceLayout.js",
+        "source/js/network/halo.js",
+        "source/js/network/hull.js",
+        "source/js/network/init.js",
+        "source/js/network/link.js",
+        "source/js/network/node.js",
+        "source/js/network/text.js",
+        "source/js/network/tick.js",
+        "source/js/svg.js",
+        "source/js/relations.js",
+        "source/js/roles.js",
+        "source/js/typeahead.js",
+        "source/js/fsm.js",
+        "source/js/init.js",
+        "source/js/index.js",
+      ],
+    },
+    emptyOutDir: true,
+    copyPublicDir: false,
+  },
+});
