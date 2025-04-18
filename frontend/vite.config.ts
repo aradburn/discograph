837b82ca9d7a9189cb0b62d8ce56d9396592e1b5
aradburn <a.radburn@btinternet.com>
2025-04-19 00:55:20 +0100

    Update coverage configuration in vite.config.ts to include source files and refine exclusions

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -39,7 +39,11 @@ export default defineConfig({
         ],
         coverage: {
             provider: "istanbul",
-            exclude: ["**/js/vendor/**", "**/frontend/node_modules/**"],
+            include: [
+                // Code in source directories
+                "source/**/*.ts",
+            ],
+            exclude: ["public/js/vendor/**", "node_modules/**"],
         },
     },
     css: {
